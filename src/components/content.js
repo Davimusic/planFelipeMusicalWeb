c1a85c0fe91059c0049e0bbbdf9dc1b1b9cfd3df
webnocode <gdfajardo6@soy.sena.edu.co>
2024-08-24 12:16:10 -0500

    quinto

--- src/components/content.js
+++ src/components/content.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import IsShow from "./IsShow";
+import IsShow from "./isShow";
 import llamarTodoAPUObjeto from "@/funciones/conectoresBackend/llamarTodoAPUObjeto";
 import { CreateSelect } from "./selects";
 import decidirTipoDeArchivo from "@/funciones/decidirTipoDeArchivo";
